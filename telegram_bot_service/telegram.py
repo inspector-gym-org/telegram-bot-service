72038bca5cf958b1a78b6d016bb9468795c094c2
Andrew Yatsura <andrewyazura203@gmail.com>
2023-04-20 02:01:30 +0300

    add default parse mode to telegram

--- telegram_bot_service/telegram.py
+++ telegram_bot_service/telegram.py
@@ -1,10 +1,14 @@
-from telegram.ext import Application
+from telegram.constants import ParseMode
+from telegram.ext import Application, Defaults
 
 from .config import settings
+
+defaults = Defaults(parse_mode=ParseMode.MARKDOWN)
 
 telegram_application = (
     Application.builder()
     .token(settings.telegram_bot_token)
     .concurrent_updates(True)
+    .defaults(defaults)
     .build()
 )
