b1b81a70023fdc52e9e94a82cd5e63726c11eed0
Andrew Yatsura <andrewyazura203@gmail.com>
2023-04-20 01:25:55 +0300

    merge training_plan/* files

--- telegram_bot_service/training_plan.py
+++ telegram_bot_service/training_plan.py
@@ -1,11 +1,52 @@
+from enum import Enum
 from uuid import UUID
 
 import requests
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 
-from ..config import settings
-from .filters import Environment, FilterEnum, Frequency, Goal, Level, Sex
+from .config import settings
+
+
+class Sex(Enum):
+    __order__ = "MALE FEMALE"
+
+    MALE = "Чоловік"
+    FEMALE = "Жінка"
+
+
+class Goal(Enum):
+    __order__ = "MUSCLE_GAIN WEIGHT_LOSS IMPROVE_HEALTH"
+
+    MUSCLE_GAIN = "Набір маси"
+    WEIGHT_LOSS = "Схуднення"
+    IMPROVE_HEALTH = "Рельєф"
+
+
+class Level(Enum):
+    __order__ = "BEGINNER MIDDLE ADVANCED"
+
+    BEGINNER = "Початківець"
+    MIDDLE = "Середній"
+    ADVANCED = "Просунений"
+
+
+class Frequency(Enum):
+    __order__ = "TWICE THRICE FOUR"
+
+    TWICE = "Двічі на тиждень"
+    THRICE = "Тричі на тиждень"
+    FOUR = "Чотири рази на тиждень"
+
+
+class Environment(Enum):
+    __order__ = "GYM HOUSE_AND_STREET"
+
+    GYM = "Спортзал"
+    HOUSE_AND_STREET = "Дім + вулиця"
+
+
+FilterEnum = Sex | Goal | Environment | Level | Frequency
 
 
 class TrainingPlan(BaseModel):
